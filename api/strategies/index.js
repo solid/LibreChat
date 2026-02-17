f998ffa6067f27a89fe825a356dc9189f9e4f03c
Precious Oritsedere <rukyjacob@gmail.com>
2026-02-17 13:50:36 +0000

    removed dupplicate imports caused by rebase

--- api/strategies/index.js
+++ api/strategies/index.js
@@ -6,16 +6,10 @@ const discordLogin = require('./discordStrategy');
 const passportLogin = require('./localStrategy');
 const googleLogin = require('./googleStrategy');
 const githubLogin = require('./githubStrategy');
-const discordLogin = require('./discordStrategy');
-const facebookLogin = require('./facebookStrategy');
-const { setupSolidOpenId, getSolidOpenIdConfig } = require('./SolidOpenidStrategy');
-const { setupOpenId, getOpenIdConfig } = require('./openidStrategy');
 const jwtLogin = require('./jwtStrategy');
 const ldapLogin = require('./ldapStrategy');
 const { setupSaml } = require('./samlStrategy');
 const appleLogin = require('./appleStrategy');
-const ldapLogin = require('./ldapStrategy');
-const jwtLogin = require('./jwtStrategy');
 
 module.exports = {
   appleLogin,
